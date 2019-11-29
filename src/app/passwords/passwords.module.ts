c675a608a5dfadcbc90ebba56bef54836cbcd9e5
mbabchenko <mbabchenko@odl1905011.lohika.com>
2019-11-29 16:13:46 +0200

    Adds base modules

---
+++ src/app/passwords/passwords.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PasswordsRoutingModule } from './passwords-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PasswordsRoutingModule
+  ]
+})
+export class PasswordsModule { }
