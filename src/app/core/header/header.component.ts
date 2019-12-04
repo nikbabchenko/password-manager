05d1ca61383f811d7968388dd263dc56281ba5ae
mbabchenko <mbabchenko@ip-172-23-109-29.us-west-2.compute.internal>
2019-12-04 11:29:23 +0200

    removes odd code from header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -16,41 +16,4 @@ export class HeaderComponent implements OnInit {
     await this.router.navigateByUrl('/');
     this.authService.logout();
   }
-
-  async downloadFile() {
-    const resp = await fetch('https://cspmarketplace.s3.us-east-2.amazonaws.com/media-files/lorem-ipsum.pdf');
-    this.saveFileFromBlob(await resp.blob(), 'name.pdf');
-  }
-
-  saveFileFromBlob = (blob: Blob | ReadableStream, fileName: string): void => {
-    // IE doesn't allow using a blob object directly
-    // instead it is necessary to use msSaveOrOpenBlob
-    // https://stackoverflow.com/questions/44824034/not-allowed-to-load-blob-pdf-resource-in-microsoft-edge
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, fileName);
-      return;
-    }
-
-    const URL = window.URL || (window as any).webkitURL;
-    const blobUrl = URL.createObjectURL(blob);
-
-    const link = document.createElement('a');
-    // have to add to document for firefox
-    link.style.display = 'none';
-    document.body.appendChild(link);
-
-    link.href = blobUrl;
-    link.download = fileName;
-    link.click();
-
-    /**
-     * Firefox, especially old versions require to have a small timeout before removing ObjectURL
-     */
-    setTimeout(() => {
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(URL);
-    }, 100);
-
-    return;
-  };
 }
